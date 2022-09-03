1efa03f995c9e9185dae707bc681b04bdad54508
Gandhi-Rajendran <ksrmgandhiraj@gmail.com>
2022-09-03 15:58:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import StudentForm from "./component/StudentForm";
 
-function App() {
+import { Wrapper } from "./component/Styled";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      defaultProps: {
+        disableElevation: true,
+        disableFocusRipple: true,
+        disableRipple: true,
+        size: "medium",
+      },
+    },
+    MuiTextField: {
+      defaultProps: {
+        size: "medium",
+      },
+    },
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <h2>Student Login Form</h2>
+        <StudentForm />
+      </Wrapper>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
